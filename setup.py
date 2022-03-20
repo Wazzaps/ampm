f4279e273c3ecdc7cb203537d54974431b5c60b8
danieleliad <daniel.eliad32@gmail.com>
2022-03-20 11:27:19 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='downloader',
+    version='1.0.0',
+    packages=['downloader'],
+    url='',
+    license='',
+    author='baba',
+    author_email='',
+    description='downloader'
+)
