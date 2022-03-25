be68c2c2fe25772a25659c82ce89b1d56efde532
David Shlemayev <david.shlemayev@gmail.com>
2022-03-25 21:23:43 +0300

    run docker container without tty

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 cd "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 rm -rf ./dist ./build
-docker run --rm -it --env=PYTHONPATH='/code/vendor' --env=GITHUB_WORKSPACE='/code' -v "$PWD":/code wazzaps/pyinstaller-manylinux-py38 --clean --noconfirm --name=ampm --add-binary /usr/local/lib/libcrypt.so.2:. /code/ampm/cli.py
+docker run --rm --env=PYTHONPATH='/code/vendor' --env=GITHUB_WORKSPACE='/code' -v "$PWD":/code wazzaps/pyinstaller-manylinux-py38 --clean --noconfirm --name=ampm --add-binary /usr/local/lib/libcrypt.so.2:. /code/ampm/cli.py
 strip ./dist/ampm/*so* ./dist/ampm/ampm ./dist/ampm/lib-dynload/*
 cp ./ampm/ampm.sh ./dist/ampm/ampm.sh
 cp ./ampm/get_ampm.sh ./dist/get_ampm.sh
