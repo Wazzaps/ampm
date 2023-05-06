3f69fda3c7e8df1b7ae6aa48fa84b22917b8b368
David Shlemayev <david.shlemayev@gmail.com>
2023-05-06 19:26:32 +0300

    force locale to fix running under certain distros

--- ampm/ampm.sh
+++ ampm/ampm.sh
@@ -4,9 +4,9 @@
 # It handles only `get` and `env` commands, with exact TYPE:HASH queries
 
 if [ "$#" -eq 2 ] && [ "$1" = "get" ] && echo "$2" | grep -P '^.*:[a-z0-9]{32}$' > /dev/null; then
-  readlink "$(echo "$2" | sed -E 's|^(.*):([a-z0-9]{32})$|/var/ampm/metadata/\1/\2.target|')" 2>/dev/null || "/opt/ampm/ampm" "$@"
+  readlink "$(echo "$2" | sed -E 's|^(.*):([a-z0-9]{32})$|/var/ampm/metadata/\1/\2.target|')" 2>/dev/null || LC_ALL=en_US.UTF-8 "/opt/ampm/ampm" "$@"
 elif [ "$#" -eq 2 ] && [ "$1" = "env" ] && echo "$2" | grep -P '^.*:[a-z0-9]{32}$' > /dev/null; then
-  cat "$(echo "$2" | sed -E 's|^(.*):([a-z0-9]{32})$|/var/ampm/metadata/\1/\2.env|')" 2>/dev/null || "/opt/ampm/ampm" "$@"
+  cat "$(echo "$2" | sed -E 's|^(.*):([a-z0-9]{32})$|/var/ampm/metadata/\1/\2.env|')" 2>/dev/null || LC_ALL=en_US.UTF-8 "/opt/ampm/ampm" "$@"
 else
-  "/opt/ampm/ampm" "$@"
+  LC_ALL=en_US.UTF-8 "/opt/ampm/ampm" "$@"
 fi
